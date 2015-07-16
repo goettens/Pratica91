5586be7e9e946655308b024b7b6d5e6f043d523b
goettens <goettens@hotmail.com>
2015-07-15 21:51:44 -0300

    Commit1

---
+++ src/main/java/Pratica91.java
@@ -0,0 +1,19 @@
+/**
+ * UTFPR - Universidade Tecnológica Federal do Paraná
+ * DAINF - Departamento Acadêmico de Informática
+ * 
+ * Template de projeto de programa Java usando Maven.
+ * @author Wilson Horstmeyer Bogado <wilson@utfpr.edu.br>
+ */
+public class Pratica91 {
+    public static void main(String[] args) {
+        Runtime runTime = Runtime.getRuntime();
+        int divisor = 1048576; //1024*1024
+        String os = System.getProperty("os.name");
+        System.out.println("Ambiente de execução: " + os);
+        System.out.println("Nº de processadores: " + runTime.availableProcessors());
+        System.out.println("Memória total em MB: " + runTime.totalMemory()/divisor);
+        System.out.println("Memória livre em MB: " + runTime.freeMemory() /divisor);
+        System.out.println("Memória máxima VirtualMachine em MB: " + runTime.maxMemory() /divisor);
+    }
+}
